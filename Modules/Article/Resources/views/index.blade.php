e51e0f151664fb8b61b8a53f0a3197a7d5ecafc4
Stanley Dany Pierre <stanleydanypierre@gmail.com>
2021-02-03 14:29:51 -0500

    Update index.blade.php

--- Modules/Article/Resources/views/index.blade.php
+++ Modules/Article/Resources/views/index.blade.php
@@ -17,18 +17,38 @@ Publication
                 <input type="date" class="form-control" name="datepublication" value="@if(isset($datepublication) && !empty($datepublication)){{ $datepublication }}@endif">
             </div>
         </div>
+        <br>
         <button class="btn btn-primary"> Apply filter</button>
         <a href="{{ url('article') }}" class="btn btn-info"> Clear filter</a>
     </form>
 </div>
 
+<style>
+  ._3lds {
+    border: 2px solid;
+    border-radius: 4px;
+    display: inline-block;
+    flex-shrink: 0;
+    height: 4px;
+    margin: 0 6px 1px 0;
+    width: 4px;
+}
+.showme {
+  display: none;
+  
+}
+
+.showhim:hover .showme {
+  display: block;
+}
+</style>
 
 <table class="table">
   <thead>
     <tr>
       <th scope="col"></th>
-      <th scope="col"></th>
-      <th scope="col"></th>
+      <th scope="col">Post status</th>
+      <th scope="col">Photos</th>
       <th scope="col"></th>
       <th scope="col"></th>
     </tr>
@@ -41,24 +61,39 @@ Publication
       <tr style="background-color: #eeeeee;">
         <td> {{ $category['name'] }}</td>
         <td></td>
-        <td> Date de publication </td>
         <td></td>
         <td></td>
+        <td></td>
+        
       </tr>
       
       @foreach($category['articles'] as $article)
       <tr>
-        <td style="font-size: 14px;"> <strong> <a href="#"> {{ $article->title }} </a> </strong> </td>
+        <td>
+          <div class="showhim" style="height:50px;">
+            <p> <strong> <a href="#" style="font-size:14px!important;font-weight:600; margin-bottom:10px;"> {{ $article->title }} </a> </strong> </p>
+          <div class="showme">
+            <a href=""> Modifier </a> | <a href="" style="color:red;"> Corbeille </a> | <a href=""> Afficher </a>
+          </div>
+        </div>
+        </td>
         <td>
           @if($article->is_active == 'TRUE')
-            {{ 'Published' }}
+            {!! '<span class="_3lds" style="border-color: rgb(66, 183, 42); background-color: rgb(66, 183, 42);"></span>Published' !!}
           @else
             {{ 'Unpublished' }}
           @endif
         </td>
-        <td>{{ date('m/d/Y', strtotime($article->publication_date)) }}</td>
-        <td>{{ $article->surtitre }}</td>
-        <td> <a href="{{ url('article/add-photo/'.$article->id) }}">add photo</a></td>
+        <td>
+          <a href="{{ url('article/add-photo/'.$article->id) }}">
+            <span class="badge badge-danger">
+				    0
+              </span>
+            </a>
+        </td>
+        <td>
+        <a href=""> Modifier </a> | <a href="" style="color:red;"> Corbeille </a> | <a href=""> Afficher </a>
+        </td>
       </tr>
       @endforeach
       
